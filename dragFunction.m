ab46fac6ce00389b0b83e06e78247c6b4c84ce5c
leomue1 <leo.muehlstrasser@tum.de>
2023-09-27 10:44:10 +0200

    aircraft data

--- dragFunction.m
+++ dragFunction.m
@@ -15,6 +15,24 @@ function [skin, form, misc, induced] = dragFunction(aircraft, altitude, velociti
 % Code History
 %   https://github.com/ngiersetum/tmal02_lab3
 
+%% Technical data A340-300
+l = 63.60; % length in [m]
+s = 60.3; % spanwidth in [m]
+h = 16.80; % height in [m]
+s_wing = 363.1; % wing reference are in [m^2]
+AR = 10;
+sweep_angle = 29.7; %[°]
+l_fuse = 5.64; % fuselage outside width in [m]
+
+
+
+%% Technical data CFM International CFM56-5C
+l_eng = 2.622; % [m]
+w_eng = 1.908; % [m]
+h_eng = 2.25; % [m]
+d_fan = 1.84; % [m]
+%Takeoff thrust of one engine = 138.78–151.24 kN
+%%
     [T, P, rho, a, mu] = ISAfunction(altitude);     % atmospheric conditions
 
     for i=1:numel(velocities)
