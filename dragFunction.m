0fe7033650ceb0e1cddc53e058f4366eeff2f220
niklas gierse <101125611+ngiersetum@users.noreply.github.com>
2023-09-28 19:17:52 +0200

    added some parentheses and small fix to s_wet_nacelle

--- dragFunction.m
+++ dragFunction.m
@@ -15,29 +15,30 @@ function [parasitic, induced] = dragFunction(altitude, velocities)
 % Code History
 %   https://github.com/ngiersetum/tmal02_lab3
 
-load('data/actable.mat')
+    load('data/actable.mat')
 
-%% Technical data A340-300
-l = table2array(acdata("A340-300", "Fuse_Length")); % length in [m]
-l_cockpit = 6; % [m]
-l_empennage = 12; % [m]
-s_wing = 363.1; % wing reference are in [m^2]
-AR = table2array(acdata("A340-300", "AspectRatio"));
-r_fuse = table2array(acdata("A340-300", "Fuse_Width"))/2; % fuselage outside width in [m]
-slant_height_cockpit = sqrt(r_fuse^2 + l_cockpit^2);
-slant_height_empennage = sqrt(r_fuse^2 + l_empennage^2);
+    %% Technical data A340-300
+    l = table2array(acdata("A340-300", "Fuse_Length")); % length in [m]
+    l_cockpit = 6; % [m]
+    l_empennage = 12; % [m]
+    s_wing = 363.1; % wing reference are in [m^2]
+    AR = table2array(acdata("A340-300", "AspectRatio"));
+    r_fuse = table2array(acdata("A340-300", "Fuse_Width"))/2; % fuselage outside width in [m]
+    slant_height_cockpit = sqrt(r_fuse^2 + l_cockpit^2);
+    slant_height_empennage = sqrt(r_fuse^2 + l_empennage^2);
 
-s_wet_wing = 2.25 * s_wing;
-%s_wet_fuse = (2*pi*l*r_fuse + 2 * pi*r_fuse^2) + (pi*r_fuse^2 + pi * r_fuse * slant_height_cockpit) + (pi*r_fuse^2 + pi * r_fuse * slant_height_empennage);
-s_wet_fuse = 2*pi*l*r_fuse + pi * r_fuse * slant_height_cockpit + pi * r_fuse * slant_height_empennage;
-s_wet_nacelle = 4 * ((pi*(table2array(acdata("A340-300", "NacelleWidth"))/2)^2) + (2*pi*table2array(acdata("A340-300", "NacelleWidth")) * 0.5 * table2array(acdata("A340-300", "NacelleLength"))));
-s_wet_tvert = 2.25 * table2array(acdata("A340-300", "TailVertArea"));
-s_wet_thor = 2.25 * table2array(acdata("A340-300", "TailHorArea"));
-
+    s_wet_wing = 2.25 * s_wing;
+    % s_wet_fuse = (2*pi*l*r_fuse + 2 * pi*r_fuse^2) + (pi*r_fuse^2 + pi * r_fuse * slant_height_cockpit) + (pi*r_fuse^2 + pi * r_fuse * slant_height_empennage);
+    s_wet_fuse = 2*pi*(l - l_cockpit - l_empennage)*r_fuse + pi * r_fuse * slant_height_cockpit + pi * r_fuse * slant_height_empennage;
+    s_wet_nacelle = ((pi*(table2array(acdata("A340-300", "NacelleWidth"))/2)^2) + (pi*table2array(acdata("A340-300", "NacelleWidth")) * table2array(acdata("A340-300", "NacelleLength"))));
+    s_wet_tvert = 2.25 * table2array(acdata("A340-300", "TailVertArea"));
+    s_wet_thor = 2.25 * table2array(acdata("A340-300", "TailHorArea"));
     
     %% Parameters for induced drag
 
     e = 0.776;   % Oswald Efficiency Number
+
+    %% Drag Calculation
 
     [T, P, rho, a, mu] = ISAfunction(altitude);     % atmospheric conditions
 
@@ -93,9 +94,9 @@ s_wet_thor = 2.25 * table2array(acdata("A340-300", "TailHorArea"));
 
         % Form Drag
         FF_fuse = (1 + 5/(f_fuse^(1.5)) + f_fuse/400);
-        FF_wing = (1 + (0.6/xc_wing) * tc_wing + 100 * tc_wing^4) * (1.34 * mach^(0.18) * cos(phi_wing)^(0.28));
-        FF_tvert = (1 + (0.6/xc_tvert) * tc_tvert + 100* tc_tvert^4) * (1.34 * mach^0.18 * cos(phi_tvert)^0.28);
-        FF_thor = (1 + (0.6/xc_thor) * tc_thor + 100* tc_thor^4) * (1.34 * mach^0.18 * cos(phi_thor)^0.28);
+        FF_wing = (1 + (0.6/xc_wing) * tc_wing + 100 * tc_wing^4) * (1.34 * mach^(0.18) * (cos(phi_wing))^(0.28));
+        FF_tvert = (1 + (0.6/xc_tvert) * tc_tvert + 100* tc_tvert^4) * (1.34 * mach^(0.18) * (cos(phi_tvert))^(0.28));
+        FF_thor = (1 + (0.6/xc_thor) * tc_thor + 100* tc_thor^4) * (1.34 * mach^(0.18) * (cos(phi_thor))^(0.28));
         FF_nacelle = 1 + (0.35 / f_nacelle);
         
 
